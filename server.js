c0e26634836c34c2b5d6739aa14842081fb1d9ec
jabbar memon <memonjabbar2@gmail.com>
2018-05-11 18:21:48 +0530

    herokupath added

--- server.js
+++ server.js
@@ -197,8 +197,7 @@ app.post("/charge", (req, res) => {
       else if(charge!=null){
         console.log(charge);
         // call email sent
-        res.redirect('http://localhost:4200/mail/ch_1CPO7uBOPVtYY8u14pSvR0kV'
-      );
+        res.redirect('https://stripepaymentdonation.herokuapp.com/mail/' +'ch_1CPO7uBOPVtYY8u14pSvR0kV');
 
         // res.status(200).json(charge);
        
