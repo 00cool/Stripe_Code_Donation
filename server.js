3bd20ae40c2ad19c310a6ef3842b7f7249258726
jabbar memon <memonjabbar2@gmail.com>
2018-05-11 17:17:07 +0530

    changes in server

--- server.js
+++ server.js
@@ -197,9 +197,10 @@ app.post("/charge", (req, res) => {
       else if(charge!=null){
         console.log(charge);
         // call email sent
-        res.redirect('http://localhost:4200/mail/' + charge.id)
-        res.status(200).json(charge);
+        res.redirect(301,'https://stripepaymentdonation.herokuapp.com//mail/' + charge.id)
         res.end();
+        res.status(200).json(charge);
+        
       }
     });
   });
